6b0f9e5d0c6a5a49c2e4030736fd8aaae8f9ed65
Egor Kloos <egor+git@lunatech.com>
2012-08-29 18:06:25 +0200

    Comments and date behaviour during browsing the calendar

--- scripts/dater.js
+++ scripts/dater.js
@@ -1,6 +1,23 @@
 /*  ###########################################################################
-    Author:     
-    Project:    
+    
+    Source: https://github.com/dutchcelt/dater
+    
+    Copyright (C) 2011 - 2012,  Lunatech Labs B.V., C. Egor Kloos. All rights reserved.
+    GNU General Public License, version 3 (GPL-3.0)
+    
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see http://www.opensource.org/licenses/gpl-3.0.html
+    
     ########################################################################### */
 
 
@@ -21,71 +38,99 @@
             return this.each(function(index,domElem) {
             
                 var $elem       = $(this),
+                                  /* The base template for a single instance datepicker */
                     $template   = $('<div class="dater-widget" id="daterWidget'+index+'"><header><a class="dater-year-previous">&#9668;</a><span></span><a class="dater-year-next">&#9658;</a></header><aside></aside><section></section><footer><a class="dater-today">today</a></footer></div>'),
                     defaults    = { today: new Date.today(),
                                     thisDate: ($elem.val()==="") ? Date.today() : Date.parse($elem.val()),
                                     format: "dd-MM-yyyy",
-                                    placeholder: ""
+                                    placeholder: "",
+                                    startDateID: "",
+                                    endDateID: ""
                                   },
                     options     = $.extend(defaults, settings);
                 
+                                  /* Return a date as a string */  
                 var dateStr     = function(date,format){
                                     return date.toString(format);
                                   },
+                                  /* Return the index of a date object. Set the format to either dd, MM or yyyy */
                     dateIndex   = function(date,format){
                                     return Number(date.toString(format))-1;
                                   },
+                    setDate     = options.thisDate.clone(),
+                                  /* Detach the datepicker from the DOM */
                     fadeOut     = function($widget){
                                     $widget = ($widget) ? $widget : $template;
                                     $widget.fadeOut('fast',function(){
                                         $(this).detach();
                                     });
                                     rendered = false;
-                                  },
+                                    if($elem.val()!="") {
+                                        options.thisDate = Date.parse($elem.val());
+                                        setDate = Date.parse($elem.val());
+                                    }
+                                 },
                     monthIndex  = Date.getMonthNumberFromName(options.thisDate.toString('MMMM')),
                     timer,
                     rendered    = false;
                     
-                    
                 var updatePicker = function(elem,selector){
-                    highlightDate(options.thisDate);
                     $elem.val(dateStr(options.thisDate,options.format));
                     elem.addClass('active');
+                    highlightDate(options.thisDate);
                 }
+                
                 var render = function($instance,f){
-                    $(".dater-widget:not('daterWidget"+index+"')").detach();
+                
+                    //  Clear all other datepickers, except this one
+                    $(".dater-widget:not('#daterWidget"+index+"')").detach();
                     var calMonths   = "",
                         calDays     = "";
+                    //  Creat a list of the Abbriviated month names
                     for (n in Date.CultureInfo.abbreviatedMonthNames) {
                         calMonths += '<a data-month="'+n+'">'+Date.CultureInfo.abbreviatedMonthNames[n]+'</a>';
                     }
+                    //  Figure out what day it is
                     var getDay = function(day){
                         var date = (day < 10 ? '0' : '') + day + dateStr(options.thisDate,".MMM.yyyy");
                         var dayNum = Date.getDayNumberFromName(Date.parse(date).toString("ddd"));
                         return dayNum;
                     }
+                    //  Ceate a list of days of the week starting with Monday
                     for (n = 0, l = Date.CultureInfo.firstLetterDayNames.length; n < l; n++) {
                         calDays += '<span>'+Date.CultureInfo.firstLetterDayNames[((n==6)?0:n+1)]+'</span>';
                     }
+                    //  Create all the days of the month
                     for (x=0,l=Date.getDaysInMonth(dateIndex(options.thisDate,'yyyy')+1,dateIndex(options.thisDate,'MM')); x<l; x++) {
                         calDays += '<a data-day="'+getDay(x+1)+'">'+(x+1)+'</a>';
                     }
+                    //  Add the Year to the template
                     $('header span',$template).html(dateStr(options.thisDate,"yyyy"));
+                    //  Add all the calnendar days to the template
                     $('section',$instance).html(calDays);
+                    //  Add the Months to the template
                     $('aside',$instance).html(calMonths);
+                    //  Add the template to the the body
                     $('body').append($instance);
-                    $instance.hide();   
+                    //  Hide the template
+                    if(!rendered){ $instance.hide(); }
+                    //  Set the first day of the month (using CSS to set its position)
                     $('section a:first').addClass('first');
                     highlightDate(options.thisDate);
                     setPos($instance);
+                    //  Callback
                     if (typeof f == "function") { f(); }
                 }
+                
                 var highlightDate = function(date){
                     $('[data-day],[data-month]',$template).removeClass('active');
-                    $('[data-day]',$template).eq(dateIndex(date,"dd")).addClass('active');
+                    console.log(dateStr(date,"MM.yyyy") === dateStr(setDate,"MM.yyyy"))
+                    if(dateStr(date,"MM.yyyy") === dateStr(setDate,"MM.yyyy")) {
+                        $('[data-day]',$template).eq(dateIndex(date,"dd")).addClass('active');
+                    }
                     $('[data-month]',$template).eq(dateIndex(date,"MM")).addClass('active');
                     $('header span',$template).html(dateStr(options.thisDate,"yyyy"));
-                    clearTimeout(timer);
+                    clearTimeout(timer); // Prevent the datepicker from detaching
                     }
                 var setPos = function($instance){
                     var offset = $elem.offset();
@@ -98,30 +143,28 @@
                 }
                 
                 //  EVENTS
-                $template.on('click',function(e){
-                    e.stopPropagation();
-                });
+                //  User clicks 'Today' and is done
                 $template.on('click','.dater-today',function(e){
                     e.preventDefault();
                     $elem.val(dateStr(options.today,options.format));
                     highlightDate(options.today);
                     fadeOut($(e.delegateTarget));
-                    return false;
                 });
+                //  User clicks on a day and is done
                 $template.on('click','[data-day]',function(e){
                     e.preventDefault();
                     options.thisDate.set({day:Number($(this).text())});
                     updatePicker($(this));
                     fadeOut($(e.delegateTarget));
-                    return false;
                 });
+                //  User clicks on a month and can continue
                 $template.on('click','[data-month]',function(e){
                     e.preventDefault();
                     options.thisDate.set({month:Date.getMonthNumberFromName($(this).text())});
-                    updatePicker($(this));
+                    render($template);
                     $elem.focus();
-                    return false;
                 });
+                //  User sets year and can continue 
                 $template.on('click','header a',function(e){
                     e.preventDefault();
                     if($(this).is('.dater-year-next')){
@@ -129,24 +172,27 @@
                     } else {
                         options.thisDate.addYears(-1);
                     }
-                    $elem.val(dateStr(options.thisDate,options.format));
-                    highlightDate(options.thisDate);
+                    $('header span',$template).html(dateStr(options.thisDate,"yyyy"));
+                    render($template);
                     $elem.focus();
                 });
+                //  Pointer device leaves the datepicker: Wait and then detach the datepicker
                 $template.on('mouseout',function(e){
                     e.preventDefault();
                     timer = setTimeout(fadeOut,800);
                 });
+                //  Pointer device (re)enters the datepicker: Prevent detaching the datepicker
                 $template.on('mouseover',function(e){
                     e.preventDefault();
-                    console.log('over');
                     clearTimeout(timer);
                     
                 });
-                $elem.on('focus focus.dater',function(e){
+                //  Input element is focus, show the datepicker
+                $elem.on('focus.dater',function(e){
                     $(this).trigger('render.dater');
                 });
-                $elem.on('click render.dater',function(e){
+                //  Render the detepicker
+                $elem.on('render.dater',function(e){
                     e.preventDefault();
                     if($("#daterWidget"+index).is(":visible")==false){
                         render($template, function(){
@@ -155,9 +201,11 @@
                         });
                     }
                 });
+                //  Set the input value
                 $elem.on('change',function(e){
                     options.thisDate = Date.parse($(this).val());
                 });
+                //  Hide the datepicker
                 $elem.on('mouseout',function(e){
                     timer=setTimeout(fadeOut, 800);
                 });
